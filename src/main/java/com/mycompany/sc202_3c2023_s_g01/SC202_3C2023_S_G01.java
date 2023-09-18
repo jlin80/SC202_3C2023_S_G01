6ac46ce58be8d701f1ee69732a60fd788e42040a
K6 <K6@DESKTOP-3PLLQH8>
2023-09-17 18:58:41 -0600

    first commit

---
+++ src/main/java/com/mycompany/sc202_3c2023_s_g01/SC202_3C2023_S_G01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sc202_3c2023_s_g01;
+
+/**
+ *
+ * @author K6
+ */
+public class SC202_3C2023_S_G01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
