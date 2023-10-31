75b6dcd1e840945d418a49136e7445456ea80708
jlin80 <145219100+jlin80@users.noreply.github.com>
2023-10-31 17:35:27 -0600

    Update SC202_3C2023_S_G01.java

--- src/main/java/com/mycompany/sc202_3c2023_s_g01/SC202_3C2023_S_G01.java
+++ src/main/java/com/mycompany/sc202_3c2023_s_g01/SC202_3C2023_S_G01.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+//Integrantes:
+//Lesly_Otavalo
+//Erick_Benavides
+//Hernán_Lu
+//Jin_Lin
 
 package com.mycompany.sc202_3c2023_s_g01;
-
-/**
- *
- * @author K6
- */
 public class SC202_3C2023_S_G01 {
 
     public static void main(String[] args) {
