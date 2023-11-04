395bae372594696be2b63808b25daefdb7720e72
Lesly <otavaloaguilarleslyyarina@gmail.com>
2023-11-03 19:13:43 -0600

    primer intento de citas

---
+++ src/main/java/com/mycompany/sc202_3c2023_s_g01/Cita.java
@@ -0,0 +1,76 @@
+package com.mycompany.sc202_3c2023_s_g01;
+public class Cita {
+    private String nombreCliente;
+    private String numTelCliente;
+    private String dia;
+    private int horaInicio;
+    private int horaFin;
+    private Barbero barberoAsignado;
+    private double precioPorHora;
+
+    public Cita(String nombreCliente, String numTelCliente, String dia, int horaInicio, int horaFin, Barbero barberoAsignado, double precioPorHora) {
+        this.nombreCliente = nombreCliente;
+        this.numTelCliente = numTelCliente;
+        this.dia = dia;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+        this.barberoAsignado = barberoAsignado;
+        this.precioPorHora = precioPorHora;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getNumTelCliente() {
+        return numTelCliente;
+    }
+
+    public void setNumTelCliente(String numTelCliente) {
+        this.numTelCliente = numTelCliente;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public int getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(int HoraInicio) {
+        this.horaInicio = HoraInicio;
+    }
+
+    public int getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(int HoraFin) {
+        this.horaFin = HoraFin;
+    }
+
+    public Barbero getBarberoAsignado() {
+        return barberoAsignado;
+    }
+
+    public void setBarberoAsignado(Barbero barberoAsignado) {
+        this.barberoAsignado = barberoAsignado;
+    }
+
+    public double getPrecioPorHora() {
+        return precioPorHora;
+    }
+
+    public void setPrecioPorHora(double precioPorHora) {
+        this.precioPorHora = precioPorHora;
+    }
+}
