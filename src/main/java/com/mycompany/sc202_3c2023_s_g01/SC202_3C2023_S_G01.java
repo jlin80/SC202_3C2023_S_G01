837e5bc2b4641069c7fb3cd11b463e4bf933d693
K6 <K6@DESKTOP-3PLLQH8>
2023-11-03 21:48:02 -0600

    Commit de Hernan

--- src/main/java/com/mycompany/sc202_3c2023_s_g01/SC202_3C2023_S_G01.java
+++ src/main/java/com/mycompany/sc202_3c2023_s_g01/SC202_3C2023_S_G01.java
@@ -1,22 +1,29 @@
 package com.mycompany.sc202_3c2023_s_g01;
 import javax.swing.JOptionPane;
-import static javax.swing.JOptionPane.showMessageDialog;
 
 public class SC202_3C2023_S_G01 {
     
     public static void main(String[] args) {
 
 //funciones de la barberia.
+
         String NombreCompleto;
         String citas;
+
+// Opcion de menu.
+
         String opcion;
 
+// salario de los servicios.
 
         int salario =  2500;
         int salario2 =  3000;
-        int MontoTotal =  salario * 13;
 
-citas = NombreCompleto;
+// iva funcion del 13 por ciento.
+
+        int MontonTotal =  salario * 13;
+
+
 
 while (true) {
     opcion = JOptionPane.showInputDialog("Digite el tipo de servicio que desea:\n"
@@ -26,40 +33,41 @@ while (true) {
                     + "4. Precio de servicio\n"
                         + "5. Salir\n");
 
-                          
+
 // Aquí puedes agregar la lógica para gestionar la cita según la opción seleccionada
 
 switch (opcion) {
     case "1":
         JOptionPane.showMessageDialog(null, "Bienvenido a nuestro menú de citas.");
-            NombreCompleto = JOptionPane.showInputDialog("Digite su nombre para guarda la cita:");    
-            citas JOptionPane.showMessageDialog(null, "Usted fue guardado con Éxito" );
+            NombreCompleto = JOptionPane.showInputDialog("Digite su nombre para guarda la cita:" );
+                JOptionPane.showMessageDialog(null, "Usted fue guardado con Éxito" );
 
-            break;
-    
+                    break;
+
     case "2":
         // Lógica para el servicio de corte con barba
             break;
-                            
+
     case "3":
         // Lógica para el servicio completo
             break;
 
     case "4":
         // Lógica para consultar el precio del servicio
-        JOptionPane.showMessageDialog(null,"Su Monto total es el siguiente :" + "\n" + MontoTotal);
+        JOptionPane.showMessageDialog(null,"Su Monto total es el siguiente :" + "\n" + 
+        MontonTotal);
             break;
 
     case "5":
         JOptionPane.showMessageDialog(null, "Saliendo del sistema");
             System.exit(0);
-                           
+
     default:
         JOptionPane.showMessageDialog(null, "Opción inválida. Digite una opción válida.");
     break;
 
-   
-            }   
+
+                }
+            }
         }
     }
-}
