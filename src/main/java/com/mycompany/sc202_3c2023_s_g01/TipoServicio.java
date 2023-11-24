9452126008122f8270aa2132a5fb38a33c1892a1
Crowned08 <87036156+Crowned08@users.noreply.github.com>
2023-11-24 14:06:01 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/sc202_3c2023_s_g01/TipoServicio.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.proyectofinal;
+
+
+public class TipoServicio {
+    public enum EleccionServicio {
+    CORTE,
+    TINTE,
+    SERVICIO_COMPLETO;}
+    
+    public int horasServicio;  
+    
+}
