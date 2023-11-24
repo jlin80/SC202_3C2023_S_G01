4cd7c69a9235796550a51978ccf0f58e9f28f854
Lesly <>
2023-11-23 23:28:36 -0600

    Actualizacion de la clase de la factura

--- src/main/java/com/mycompany/sc202_3c2023_s_g01/Factura.java
+++ src/main/java/com/mycompany/sc202_3c2023_s_g01/Factura.java
@@ -1,36 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.sc202_3c2023_s_g01;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author otava
- */
 public class Factura {
     
-    private Cita citaAsociada;
-    private double totalFactura;
-
-    public Factura( Cita citaAsociada, double totalFactura) {
-        this.citaAsociada = citaAsociada;
-        this.totalFactura = totalFactura;
-    }
-    public Cita getCitaAsociada() {
-        return citaAsociada;
-    }
-
-    public void setCitaAsociada(Cita citaAsociada) {
-        this.citaAsociada = citaAsociada;
-    }
-
-    public double getTotalFactura() {
-        return totalFactura;
-    }
-
-    public void setTotalFactura(double totalFactura) {
-        this.totalFactura = totalFactura;
+    public int precio = 2500;
+    public int precioFin = 3000;
+    public double totalEntre = 0;
+    public double totalFinde =0;
+    public String fecha="";
+    
+    
+    
+    public void muestraFactura (int dia,int duracion,String nombreCliente, String numTelCliente){
+    
+    if (dia <=5) {
+        fecha = JOptionPane.showInputDialog(null,"indigue la fecha de la factura");
+        totalEntre =((precio * duracion)*0.13); 
+        JOptionPane.showMessageDialog(null, "***FACTURA***\n FECHA: "+fecha+ "\nNOMBRE DE CLIENTE  : "+ nombreCliente +"\n tTELÉFONO :"+numTelCliente+"DÍA :"+dia
+        + "TIPO DE TRABAJO :(hay que hacer una lista de por lo menos 5 tipos de trabajos que despues de le da equivalencia a duración para colocar esta parte) "+
+                "TOTAL CON IVA INCLUIDO: " + totalEntre );
+  } else 
+    {
+       totalFinde = ((precioFin * duracion)*0.13);
+       fecha = JOptionPane.showInputDialog(null,"indigue la fecha de la factura");
+       
+      JOptionPane.showMessageDialog(null, "***FACTURA***\n FECHA: "+fecha+ "\nNOMBRE DE CLIENTE  : "+ nombreCliente +"\n tTELÉFONO :"+numTelCliente+"DÍA :"+ dia
+              +"TIPO DE TRABAJO :(hay que hacer una lista de por lo menos 5 tipos de trabajos que despues de le da equivalencia a duración para colocar esta parte)"
+                + "TOTAL CON IVA INCLUIDO: " + totalFinde );
+        }
     }
 }
-
